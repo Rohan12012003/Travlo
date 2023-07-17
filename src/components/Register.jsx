8a4c50314aa6a9a68b8404a109be73881906c453
rohan <rohandutta030620@.com>
2023-07-17 20:05:03 +0530

    console.log to register.js

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-//import axios from 'axios';
+import axios from 'axios';
 
 function RegistrationPage(props) {
   const [userDetails, setUserDetails] = useState({
