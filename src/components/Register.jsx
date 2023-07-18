6efc952daf0f216e31e1e99c1df80b8d3f7b62c1
rohan <rohandutta030620@.com>
2023-07-18 12:13:03 +0530

    adding registercopy file

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -47,7 +47,7 @@ function RegistrationPage(props) {
   };
       //http://localhost:5000/Register
       //./../.netlify/functions/Register
-     fetch("./../.netlify/functions/Register",options)
+     fetch("/Register",options)
       .then(response => {
         // Handle the response from the server
         console.log(response.data); // Example: Log the response data
