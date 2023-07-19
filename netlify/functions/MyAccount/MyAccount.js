69979970e3db079d63e43bde06ccd857a294fc4d
rohan <rohandutta030620@.com>
2023-07-19 11:52:59 +0530

    adding objectid

--- netlify/functions/MyAccount/MyAccount.js
+++ netlify/functions/MyAccount/MyAccount.js
@@ -1,4 +1,4 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion,ObjectId } = require('mongodb');
 require('dotenv').config();
 
 exports.handler = async function (event, context) {
